8adcba8a7063488edbaeef155a3f2cc81bce56c4
SENAI Zerbini <aluno@escolar509.sp.senai.br>
2025-11-18 17:02:56 -0300

    V1

---
+++ app.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAqCzcbTf4ZWwQKXGY5ELD3qjxjokay6Ds",
+  authDomain: "projeto-final-bb566.firebaseapp.com",
+  projectId: "projeto-final-bb566",
+  storageBucket: "projeto-final-bb566.firebasestorage.app",
+  messagingSenderId: "964611662890",
+  appId: "1:964611662890:web:b0988263a185c92da1cf53"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
